3466ebf99128b25feb59086be18c5e779b0107ea
Rajveer Joshi <137373149+RajveerJoshi@users.noreply.github.com>
2024-02-18 08:31:38 +0530

    Update client1.java

--- client1.java
+++ client1.java
@@ -1,28 +1,53 @@
-#include<windows.h>
-#include<stdio.h>
-#include<string.h>
-int main()
+import java.io.*;
+import java.net.*;
+class Client
 {
-char response[5000];
-char request[5000];
-WORD ver;
-WSADATA wsaData;
-int serverSocketDescriptor;
-int clientSocketDescriptor;
-struct sockaddr_in serverSocketInformation;
-ver=MAKEWORD(1,1);
-WSAStartup(ver,&wsaData);
-clientSocketDescriptor=socket(AF_INET,SOCK_STREAM,0);
-serverSocketInformation.sin_family=AF_INET;
-serverSocketInformation.sin_port=htons(5050);
-serverSocketInformation.sin_addr.s_addr=inet_addr("127.0.0.1");
-connect(clientSocketDescriptor,(struct sockaddr *)&serverSocketInformation, sizeof(serverSocketInformation));
-strcpy(request,"hello i am rajveer joshi best beigginer for coding");
-send(clientSocketDescriptor,request,strlen(request)+1,0);
-recv(clientSocketDescriptor,response,sizeof(response),0);
-printf("Response received\n");
-printf("%s\n",response);
-closesocket(clientSocketDescriptor);
-WSACleanup();
-return 0;
+public static void main(String []args)
+{
+
+//data is taken as command line argument 
+try
+{
+int rollNumber=Integer.parseInt(args[0]);
+String name=args[1];
+String gender=args[2];
+String request=rollNumber+","+name+","+gender+"#";// act as a terminator
+Socket socket=new Socket("localhost",5500);
+// Socket is initialized and attempt is made for Connecting to the server
+
+// Declearing other Properties and Streams
+InputStream is;
+InputStreamReader isr;
+OutputStream os;
+OutputStreamWriter osw;
+String response;
+StringBuffer sb;
+int x;
+
+// retrieving output Stream and its witer, for sending request or acknowledgement
+os=socket.getOutputStream();
+osw=new OutputStreamWriter(os);
+osw.write(request);
+osw.flush();
+//request sent
+
+// retrieving input Stream and its reader, for receiving response or acknowledgement
+is=socket.getInputStream();
+isr=new InputStreamReader(is);
+sb=new StringBuffer();
+while(true)
+{
+x=isr.read();
+if(x==-1||x=='#') break; // read till the terminator
+sb.append((char)x);
+}
+response=sb.toString();
+System.out.println("Response is:"+response);
+socket.close(); // closing the Connection
+}catch(Exception e)
+{
+//Raised in case, Connection is refused or some other technical issue
+System.out.println(e);
+}
+}
 }
